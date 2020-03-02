0eb4a6beece6a57f62cbf1140a8d96e1df0b2974
Fabián Toro <helber.toro@gmail.com>
2020-03-02 20:06:51 +0000

    route protection

--- app/Http/Controllers/CarnetController.php
+++ app/Http/Controllers/CarnetController.php
@@ -3,15 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Carnet;
 
 class CarnetController extends Controller
 {
     public function test() {
-        return 'ok';
+        return view('test');
     }
 
     public function carneIdcbis(Request $request) {
         $data = $request->all();
-        return response()->json($data);
+        $carnet = new Carnet;
+        $carnet->fill($data);
+
+        $carnet->save();
+        return 'ok';
+        /* return response()->json($data); */
     }
 }
---
+++ resources/views/test.blade.php
@@ -0,0 +1,17 @@
+
+<form action="{!! route('carneIdcbis') !!}" method="post">
+  <input type="text" name="name" value="Helber">
+  <input type="text" name="typeIdentification" value="CC">
+  <input type="text" name="identification" value="1023887419">
+  <input type="text" name="rh" value="A+">
+  <input type="date" name="birthday" value=>
+  <input type="text" name="address" value="Call 65 # 5 - 90">
+  <input type="text" name="phone" value="3192705253">
+  <input type="text" name="email" value="htoro@idcbis.org.co">
+  <input type="text" name="profession" value="Ing. de sistemas">
+  <input type="text" name="workGroup" value="Comunicaciones">
+  <input type="text" name="picture" value="jj.jpg">
+  
+  <button type="submit">Enviar</button>
+
+</form>
