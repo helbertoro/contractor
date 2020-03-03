6e686658a7b9e007d6d049aace5529cfdb5e74a0
Fabián Toro <helber.toro@gmail.com>
2020-03-03 14:53:15 +0000

    Install intervention image

--- app/Http/Controllers/CarnetController.php
+++ app/Http/Controllers/CarnetController.php
@@ -13,11 +13,16 @@ class CarnetController extends Controller
 
     public function carneIdcbis(Request $request) {
         $data = $request->all();
+
+        $image = \Request::file('picture');
+        $filename = time() . $image->GetClientOriginalName();
+        $path = 'images/fotos_contratistas/' . $filename;
+        \Image::make($image->GetRealPath())->widen(500)->save($path);
+
         $carnet = new Carnet;
         $carnet->fill($data);
 
         $carnet->save();
         return 'ok';
-        /* return response()->json($data); */
     }
 }
